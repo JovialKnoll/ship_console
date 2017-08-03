e82aa164cdf327830be327587874e044764efdc1
JovialKnoll <BrettCKaplan@gmail.com>
2017-08-03 17:39:12 +0000

    handler set up

--- handler.py
+++ handler.py
@@ -1,12 +1,16 @@
 class Handler(object):
+    welcome_message = "test welcome message"
     def __init__(self):
         input_dict = {}
         #add to dict
-    def default():
+    def _default():
         print("default text goes here")
     def handle_input(input):
         #check for special "end everything" input
-        #maybe pass in just first word? pass other words to function?
-        function = input_dict.get(input, self.default)
+        if input == "game_over_xxx":
+             return False
+        #maybe pass in just first word?
+        #pass other words to function?
+        function = input_dict.get(input, self._default)
         function()
         return True
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-
 import sys
+from handler import *
 
 def main():
-    print("hello")
+    handler = Handler()
+    print(handler.welcome_message)
+    still_running = True
+    while (still_running):
+        #get actual input
+        input = "game_over_xxx"
+        still_running = handler.handle_input(input)
 
 if __name__ == "__main__":
     main()
