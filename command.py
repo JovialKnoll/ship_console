9dea1547c288d1270e5cb79f57002c745b988bf0
JovialKnoll <BrettCKaplan@gmail.com>
2017-08-03 13:59:14 +0000

    adding base class for commands

---
+++ command.py
@@ -0,0 +1,5 @@
+class Command(object):
+    def get_key:
+        raise NotImplementedError(self.__class__.__name__ + ".get_key")
+    def run:
+        raise NotImplementedError(self.__class__.__name__ + ".run")
