0864f191a8db2286f0782f5c39cd76369abb9bd7
JovialKnoll <BrettCKaplan@gmail.com>
2017-08-04 14:34:43 +0000

    list of commands

--- commands/command.py
+++ commands/command.py
@@ -1,9 +1,34 @@
+import os
+
 class Command(object):
+    commands_visible = []
+    for (dirpath, dirnames, filenames) in os.walk('.'):
+        for filename in filenames:
+            if (
+                not filename.endswith('.py')
+                or not filename[0].isalpha()
+                or not filename == 'command.py'
+            ):
+                continue
+            clean_name = filename[:-3]
+            commands_visible.append(clean_name)
+        break
+    commands_visible.sort()
+
+    @classmethod
+    def get_all_commands(cls):
+        return list(commands_visible)
+
+    @classmethod
+    def get_visible_commands(cls):
+        return list(commands_visible)
+
     def get_key(self):
         raise NotImplementedError(
             self.__class__.__name__
             + ".get_key"
         )
+
     def run(self, inputs):
         raise NotImplementedError(
             self.__class__.__name__
--- commands/help.py
+++ commands/help.py
@@ -1,14 +1,9 @@
 from command import *
 
 class Help(Command):
-    def __init__(self):
-        #place non-hidden commands here
-        self.command_list = [
-            "help"
-        ]
     def get_key(self):
         return "help"
     def run(self, inputs):
         print("commands:")
-        for command in command_list:
+        for command in self.get_visible_commands():
             print(command)
